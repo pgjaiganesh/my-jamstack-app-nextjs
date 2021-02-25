1a32bb6a834305f32452721e1e744b7924d9a5c9
pgjaiganesh <jaiganesh.girinathan@gmail.com>
2021-02-25 19:00:39 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a> from Amplify 3.0
+          Welcome to Hackathon <a href="https://nextjs.org">Next.js!</a> from Amplify 3.0
         </h1>
 
         <p className={styles.description}>
